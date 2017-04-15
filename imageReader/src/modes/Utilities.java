cafaa165a253fc0c546cc03893b0919abda9aaca
Liam Da Silva <daxs3130@mylaurier.ca>
2017-04-15 00:14:04 -0400

    Added signing, signature extraction and bit shifting functions to Utilities.

--- imageReader/src/modes/Utilities.java
+++ imageReader/src/modes/Utilities.java
@@ -20,6 +20,8 @@ public class Utilities {
 	//the number of bytes an integer takes up (used to encode file length at start)
 	private static final int INT_BYTES = 32;
 	
+	private static final String SIGNATUREHEADER = "0000000011111111";
+	
 	/**
 	 * Creates a BufferedImage object from an image filename/path
 	 * @param fileName the image filename
@@ -95,6 +97,197 @@ public class Utilities {
 		}
 	}
 	
+	/**
+	 * Signs an image using one-bit encoding
+	 * @param imageName the name/path of the image
+	 * @param signature the signature to be encoded into the image
+	 * @param outFile the name/path of the encoded image that will be written out
+	 * @return (boolean) whether the signing was successful or not
+	 * 
+	 */
+	public static boolean signImage(String imageName, String signature, String outFile){
+		int bits = 1;
+		if(signature.length()>50||signature.length()<=0){
+			return false;
+		}
+		BufferedImage img = getBufferedImageFromFile(imageName);
+		
+		try {
+			//convert signature to binary and attach marker to start of it
+			String binString = SIGNATUREHEADER+toBinary(signature);
+			IMGWriter imgTracker = new IMGWriter(img,bits);
+			int counter = 0;
+			for (int i=0;i<img.getHeight();i++){
+				for (int j=0;j<img.getWidth();j++){
+					String nextBits = binString.substring(counter, counter+1);
+					imgTracker.insertBits(nextBits);
+					counter=(counter+1)%binString.length();
+				}
+			}
+			//get output file extension
+			String[] array = imageName.split("\\.");
+			String extension = array[array.length-1];
+			ImageIO.write(imgTracker.getIMG(), extension, new File(outFile));
+			return true;
+				
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * Gets a signature from an image
+	 * @param imageName the name/path of the image
+	 * @return (String) the signature extracted
+	 * 
+	 */
+	public static String getSignature(String imageName){
+		int bits = 1;
+		BufferedImage img = getBufferedImageFromFile(imageName);
+		
+		IMGReader imgReader = new IMGReader(img,bits);
+		String tempString = "";
+		String string = "";
+		boolean beginningFound = false;
+		boolean signatureFound = false;
+		int i =0;int j =0;
+		while (i<img.getHeight()&&!signatureFound){
+			while (j<img.getWidth()&&!signatureFound){
+				tempString+=imgReader.readBits();
+				if (!beginningFound){
+					if(tempString.contains(SIGNATUREHEADER)){//is this beginning of signature?
+						beginningFound = true;
+						tempString="";
+					}
+				}else{
+					if(tempString.contains(SIGNATUREHEADER)){//is the signature done?
+						signatureFound = true;
+						tempString=tempString.substring(0, tempString.length() - SIGNATUREHEADER.length());
+						string = binaryToString(tempString);
+					}
+				}
+				j++;
+			}
+			i++;
+		}
+		
+		return string;
+	}
+	
+    /**
+     * Converts a string to a string of its binary representation
+     * @param text the text to convert
+     * @return (String) the binary string
+     */
+    private static String toBinary(String text) {
+        StringBuilder sb = new StringBuilder();
+        for (char character : text.toCharArray()) {
+            sb.append(String.format("%8s", Integer.toBinaryString(character)).replace(' ', '0'));
+        }
+        return sb.toString();
+
+    }
+    
+    /**
+     * Converts a binary string to a string
+     * @param text the text to convert
+     * @return (String) the string
+     */
+    private static String binaryToString(String text) {
+        String result = "";
+    	if ((text.length()%8==0)){
+    		int c = 0;
+    		while (c<text.length()/8){
+    			String charCode = text.substring(0, 8);
+    			text = text.substring(8);
+    			result += new Character((char)Integer.parseInt(charCode,2));
+    		}
+    		
+    	}
+    	return result;
+
+    }
+    
+    /**
+	 * Shifts the bits in each byte 'bitsToShift' places left
+	 * @param imageName the name/path of the image
+	 * @param bitsToShift the signature to be encoded into the image
+	 * @param outFile the name/path of the image that will be written out
+	 * @return (boolean) whether the shifting was successful or not
+	 * 
+	 */
+	public static boolean shiftBits(String imageName,int bitsToShift, String outFile){
+		if(bitsToShift>8||bitsToShift<0){
+			return false;
+		}
+		BufferedImage img = getBufferedImageFromFile(imageName);
+		
+		try {
+			for (int i=0;i<img.getHeight();i++){
+				for (int j=0;j<img.getWidth();j++){
+					Color curColor = new Color(img.getRGB(j, i));
+					int r = (curColor.getRed()<<bitsToShift)%256;
+					int g = (curColor.getGreen()<<bitsToShift)%256;
+					int b = (curColor.getBlue()<<bitsToShift)%256;
+					Color newc = new Color(r,g,b,curColor.getAlpha());
+					img.setRGB(j, i, newc.getRGB());
+				}
+			}
+			//get output file extension
+			String[] array = imageName.split("\\.");
+			String extension = array[array.length-1];
+			ImageIO.write(img, extension, new File(outFile));
+			return true;
+				
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * Shifts the bits in each byte 'bitsToShift' places left
+	 * @param imageName the name/path of the image
+	 * @param bitsToShift the signature to be encoded into the image
+	 * @param outFile the name/path of the image that will be written out
+	 * @return (boolean) whether the shifting was successful or not
+	 * 
+	 */
+	public static boolean getLeastSigBits(String imageName,int bitsToGet, String outFile){
+		if(bitsToGet>8||bitsToGet<=0){
+			return false;
+		}
+		int intToAnd = (int)Math.pow(2, bitsToGet)-1;
+		BufferedImage img = getBufferedImageFromFile(imageName);
+		
+		try {
+			for (int i=0;i<img.getHeight();i++){
+				for (int j=0;j<img.getWidth();j++){
+					Color curColor = new Color(img.getRGB(j, i));
+					int r = curColor.getRed();
+					int g = curColor.getGreen();
+					int b = curColor.getBlue();
+					r = r&0b00000011;
+					g = g&0b00000011;
+					b = b&0b00000011;
+					Color newc = new Color(r,g,b,curColor.getAlpha());
+					img.setRGB(j, i, newc.getRGB());
+				}
+			}
+			//get output file extension
+			String[] array = imageName.split("\\.");
+			String extension = array[array.length-1];
+			System.out.println("Writing file...");
+			ImageIO.write(img, extension, new File(outFile));
+			System.out.println("Done");
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+    
 	
 	/**
 	 * Encodes a file into an image given the number of 'bits per byte' to use for encoding
@@ -106,7 +299,7 @@ public class Utilities {
 	 * 
 	 */
 	public static int insertFileIntoImage(String imageName, String fileName, int bits, String outFile){
-		if(bits>8||bits<0){
+		if(bits>8||bits<=0){
 			return -1;
 		}
 		BufferedImage img = getBufferedImageFromFile(imageName);
--- imageReader/src/modes/test.java
+++ imageReader/src/modes/test.java
@@ -8,47 +8,78 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.imageio.ImageIO;
 
 public class test {
 
 	public static void main(String[] args) {
-		try {
-//			File f = new File("/Users/liamdasilva/Desktop/Hello World.txt");
-			int l = (int) new File("/Users/liamdasilva/Desktop/Hello World.txt").length();
-			FileInputStream f = new FileInputStream("/Users/liamdasilva/Desktop/Hello World.txt");
-//			byte x = (byte) f.read();
-			//byte[] data = Files.readAllBytes(fileLocation);
-		    StringBuilder sb = new StringBuilder();
-		    //System.out.println(data.length);
-//		    for (byte b : data) {
-//		        sb.append(String.format("%02X ", b));
-//		    }
-//		    System.out.println(sb.toString());
-			int i=0;
-			while (i <l){
-				byte shifted = (byte) f.read();
-				byte cur = shifted;
-				System.out.println(Integer.toBinaryString(cur));
-				cur =(byte) (shifted & 0b11);
-				System.out.println(Integer.toBinaryString(cur));
-				shifted=(byte) (shifted>>2);
-				cur =(byte) (shifted & 0b00000011);
-				System.out.println(Integer.toBinaryString(cur));
-				shifted=(byte) (shifted>>2);
-				cur =(byte) (shifted & 0b00000011);
-				System.out.println(Integer.toBinaryString(cur));
-				shifted=(byte) (shifted>>2);
-				cur =(byte) (shifted & 0b00000011);
-				System.out.println(Integer.toBinaryString(cur));
-				i++;
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+//		try {
+////			File f = new File("/Users/liamdasilva/Desktop/Hello World.txt");
+//			int l = (int) new File("/Users/liamdasilva/Desktop/Hello World.txt").length();
+//			FileInputStream f = new FileInputStream("/Users/liamdasilva/Desktop/Hello World.txt");
+////			byte x = (byte) f.read();
+//			//byte[] data = Files.readAllBytes(fileLocation);
+//		    StringBuilder sb = new StringBuilder();
+//		    //System.out.println(data.length);
+////		    for (byte b : data) {
+////		        sb.append(String.format("%02X ", b));
+////		    }
+////		    System.out.println(sb.toString());
+//			int i=0;
+//			while (i <l){
+//				byte shifted = (byte) f.read();
+//				byte cur = shifted;
+//				System.out.println(Integer.toBinaryString(cur));
+//				cur =(byte) (shifted & 0b11);
+//				System.out.println(Integer.toBinaryString(cur));
+//				shifted=(byte) (shifted>>2);
+//				cur =(byte) (shifted & 0b00000011);
+//				System.out.println(Integer.toBinaryString(cur));
+//				shifted=(byte) (shifted>>2);
+//				cur =(byte) (shifted & 0b00000011);
+//				System.out.println(Integer.toBinaryString(cur));
+//				shifted=(byte) (shifted>>2);
+//				cur =(byte) (shifted & 0b00000011);
+//				System.out.println(Integer.toBinaryString(cur));
+//				i++;
+//			}
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		String imageName = "/Users/liamdasilva/Desktop/sunset/sunset.png";
+		String fileName = "/Users/liamdasilva/Desktop/HelloWorld.txt";
+		String outImage = "/Users/liamdasilva/Desktop/sunset/testing.png";
+		String outImageShiftPath = "/Users/liamdasilva/Desktop/sunset/shift/";
+		String outFile = "/Users/liamdasilva/Desktop/sunset/output.txt";
+		
+		int bitsToEncode = 1;
+		
+//		System.out.println(NumberFormat.getNumberInstance(Locale.US)
+//				.format(Utilities.getMaxBytes(imageName, bitsToEncode))+
+//				" bytes can fit in this file using "+ bitsToEncode+" bit encoding.");
+//		int result = Utilities.insertFileIntoImage(imageName,fileName,bitsToEncode,outImage);
+//		if (!(result==-1)){
+//			System.out.println(NumberFormat.getNumberInstance(Locale.US).format(result)+" bytes successfully encoded.");
+//		}
+//		int result2 = Utilities.readFileFromImage(outImage, bitsToEncode, outFile);
+//		if (!(result2==-1)){
+//			System.out.println(NumberFormat.getNumberInstance(Locale.US).format(result2)+" bytes successfully read.");
+//		}
+		
+//		Utilities.signImage(imageName, "Property of Liam Da Silva", outImage);
+//		String signature = Utilities.getSignature(outImage);
+//		System.out.println(signature);
+		
+//		Utilities.shiftBits(outImage, 1, outImageShiftPath+1+"bitsshifted.png");
+//		for (int i=2;i<=8;i++){
+//			Utilities.shiftBits(outImageShiftPath+(i-1)+"bitsshifted.png", 1, outImageShiftPath+i+"bitsshifted.png");
+//		}
+		
+		Utilities.getLeastSigBits(imageName, 1, outImage);
 	}
 
 }
